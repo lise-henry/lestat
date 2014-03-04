63bd7c194f23be3510db7b7ff6586b40eb383a15
Elisabeth Henry <liz.henry@ouvaton.org>
2014-03-04 03:56:44 +0100

    Added project.clj

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject lestat "0.1.0-SNAPSHOT"
+  :description "A tiny statistical tool analysing the importance of characters in novels"
+  :url "https://github.com/lady-segfault/lestat"
+  :license {:name "GNU General Public License V3"
+            :url "https://www.gnu.org/copyleft/gpl.html"}
+  :repositories [["erichseifert.de" "http://mvn.erichseifert.de/maven2"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [de.erichseifert.gral/gral-core "0.10"]])
