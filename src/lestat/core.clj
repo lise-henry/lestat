a61306a409bb2a1b682daf03c781d70b67d96b52
Elisabeth Henry <liz.henry@ouvaton.org>
2014-03-05 07:37:36 +0100

    core.clj: cleaned file

--- src/lestat/core.clj
+++ src/lestat/core.clj
@@ -1,28 +1,11 @@
 (ns lestat.core
   (:gen-class)
-  (:require [lestat.analysis :as analysis]
-            [lestat.gui :as gui])
-  (:import (javax.swing JFrame)
-           (com.xeiam.xchart Chart SwingWrapper StyleManager StyleManager$LegendPosition XChartPanel))
-  (:use seesaw.core))
+  (:require [lestat.gui :as gui]))
 
 (defn -main
   "Main function"
   [& args]
-;          data (analysis/data-by-floating-window text)
-;          chart (analysis/data->chart data)]
-;      (.setChartTitle ^Chart chart "Résultats")
-;      (.setLegendPosition ^StyleManager (.getStyleManager ^Chart chart) StyleManager$LegendPosition/InsideSW)
-  (gui/main-window))
-; (XChartPanel. ^Chart chart)))))))
-
-
-;; o         (-> (frame :title "Lestat"
-;;                     :on-close :exit
-;;                     :content (XChartPanel. ^Chart chart))
-;;              pack!
-;;              show!))))))
-  
+  (gui/main-window))  
   
 
 
